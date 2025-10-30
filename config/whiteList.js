4e09f06003908b2bb9785e2732581e00a08ef84d
masangakevin256 <masangakevin1@gmail.com>
2025-10-30 11:47:57 +0300

    configuring whitelist

--- config/whiteList.js
+++ config/whiteList.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
-  "http://localhost:5173",       // your React frontend
+  "http://localhost:5174",       //  React frontend
   "http://localhost:3500",
-  "http://localhost:3500" ,      // your production frontend
+  "http://localhost:3500" ,      //  production frontend
   "https://www.google.com"
 ];
 module.exports= allowedOrigins;
